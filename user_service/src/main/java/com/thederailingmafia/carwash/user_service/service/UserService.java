946f79cfb92136ba8ab9cffbaa394803dc6cdeeb
nitishsingh877 <nitishkumarsingh877@gmail.com>
2025-03-21 15:56:03 +0530

    add: added code for login endpoints and authorise via jwt tokens and put in signup too.

--- user_service/src/main/java/com/thederailingmafia/carwash/user_service/service/UserService.java
+++ user_service/src/main/java/com/thederailingmafia/carwash/user_service/service/UserService.java
@@ -1,12 +1,18 @@
 package com.thederailingmafia.carwash.user_service.service;
 
 import com.thederailingmafia.carwash.user_service.dto.CustomerDto;
+import com.thederailingmafia.carwash.user_service.dto.LoginResponseDto;
 import com.thederailingmafia.carwash.user_service.dto.UserDto;
 import com.thederailingmafia.carwash.user_service.dto.WasherDto;
 import com.thederailingmafia.carwash.user_service.model.UserModel;
 import com.thederailingmafia.carwash.user_service.model.UserRole;
 import com.thederailingmafia.carwash.user_service.repository.UserRepository;
+import com.thederailingmafia.carwash.user_service.util.JwtUtil;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,7 +29,10 @@ public class UserService {
     @Autowired
     private WasherService washerService;
 
-    public UserModel saveUser(UserDto userDto,String authToken) {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public UserModel saveUser(UserDto userDto, String authToken) {
         UserModel userModel = new UserModel(userDto.getName(), userDto.getPassword(), userDto.getEmail(), userDto.getUserRole(),null, null,authToken);
         if(authToken.equals("Email")) {
             userModel.setPassword(passwordEncoder.encode(userModel.getPassword()));
@@ -40,12 +49,26 @@ public class UserService {
             washerService.SaveWasher(userModel,washerDto);
 
         }
-
         return userModel;
+       // return jwtUtil.generateToken(userModel.getEmail(), userModel.getUserRole().name());
     }
 
+    @Transactional
+    public LoginResponseDto loginUser(String email, String password) throws Exception, AuthenticationException, UsernameNotFoundException {
+        UserModel user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new Exception("User with email " + email + " not found"));
 
+        if(!user.getAuth().equals("Email")) {
+           throw new Exception("Wrong way of login");
+        }
 
+        if(!passwordEncoder.matches(password, user.getPassword())) {
+            throw new Exception("Wrong password");
+        }
 
+        String token = jwtUtil.generateToken(user.getEmail(), user.getUserRole().name());
+        UserDto userDto = new UserDto(user.getName(), user.getEmail(), null,null,user.getUserRole());
 
+        return new LoginResponseDto(token,userDto);
+    }
 }
